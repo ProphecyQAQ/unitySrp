49d861a832c2fdedefc025ca0b6b965fed612836
Lin Chang <lilyforeverx@gmail.com>
2024-10-13 15:41:27 +0800

    Add scene

---
+++ Assets/Scripts/RandomGenerateSphere.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomGenerateSphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material[] material;
+    void Start()
+    {
+        for (int i = -2; i < 2; i ++)
+        {
+            for (int j = -2; j < 2; j ++)
+            {
+
+                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                sphere.transform.position = new Vector3(i*2, 0, j*2);
+
+                Renderer renderer = sphere.GetComponent<Renderer>();
+                renderer.material = material[Random.Range(0, 3)];
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
